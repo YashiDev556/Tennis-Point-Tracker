ba2a0b7c491ed32055d52eb9543bd5699dbec380
Yash Iyer <yashi@debian-BULLSEYE-live-builder-AMD64>
2021-12-24 19:10:13 -0500

    new file:   README.md 	new file:   app/.gitignore 	new file:   app/build.gradle 	new file:   app/proguard-rules.pro 	new file:   app/src/androidTest/java/com/example/tennispointtracker/ExampleInstrumentedTest.kt 	new file:   app/src/main/AndroidManifest.xml 	new file:   app/src/main/java/com/example/tennispointtracker/MainActivity.kt 	new file:   app/src/main/res/drawable-v24/ic_launcher_foreground.xml 	new file:   app/src/main/res/drawable/ic_launcher_background.xml 	new file:   app/src/main/res/layout/activity_main.xml 	new file:   app/src/main/res/layout/mainmenu.xml 	new file:   app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml 	new file:   app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml 	new file:   app/src/main/res/mipmap-hdpi/ic_launcher.webp 	new file:   app/src/main/res/mipmap-hdpi/ic_launcher_round.webp 	new file:   app/src/main/res/mipmap-mdpi/ic_launcher.webp 	new file:   app/src/main/res/mipmap-mdpi/ic_launcher_round.webp 	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher.webp 	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher_round.webp 	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher.webp 	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher_round.webp 	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher.webp 	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp 	new file:   app/src/main/res/values-night/themes.xml 	new file:   app/src/main/res/values/colors.xml 	new file:   app/src/main/res/values/strings.xml 	new file:   app/src/main/res/values/themes.xml 	new file:   app/src/test/java/com/example/tennispointtracker/ExampleUnitTest.kt 	new file:   build.gradle 	new file:   gradle.properties 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   local.properties 	new file:   settings.gradle

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tennis-Point-Tracker
---
+++ app/src/main/java/com/example/tennispointtracker/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.tennispointtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mainmenu) //sets the view to the main menu
+    }
+
+
+
+
+
+
+}
