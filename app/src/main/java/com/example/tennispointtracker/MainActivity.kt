1ac33a510b224d4c717ae88069b0e90995d7e4a5
Yash Iyer <yashi@debian-BULLSEYE-live-builder-AMD64>
2022-01-10 11:45:39 -0500

    modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/com/example/tennispointtracker/MainActivity.kt 	modified:   app/src/main/java/com/example/tennispointtracker/MainMenu.kt 	modified:   app/src/main/res/layout/activity_main.xml 	modified:   app/src/main/res/layout/mainmenu.xml 	modified:   build.gradle

--- app/src/main/java/com/example/tennispointtracker/MainActivity.kt
+++ app/src/main/java/com/example/tennispointtracker/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.tennispointtracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
-class MainActivity2 : AppCompatActivity() {
+import com.example.tennispointtracker.MainMenu
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val matchTitle = findViewById<TextView>(R.id.matchTitle)
+
+        if (intent != null) {
+            if (intent.extras != null) {
+                matchTitle.text = intent.extras!!.getString("MatchText")
+            }
+        }
+
+
+        val mainMenuButton = findViewById<Button>(R.id.mainMenuButton)
+
+        val mainMenuIntent = Intent(this, MainMenu::class.java)
+
+        mainMenuButton.setOnClickListener {
+            //goes back to main menu
+            startActivity(mainMenuIntent)
+        }
+
     }
 }
--- app/src/main/java/com/example/tennispointtracker/MainMenu.kt
+++ app/src/main/java/com/example/tennispointtracker/MainMenu.kt
@@ -1,14 +1,13 @@
 package com.example.tennispointtracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainmenu) //sets the view to the main menu
@@ -20,7 +19,8 @@ class MainActivity : AppCompatActivity() {
 
 
         val matchNameField = findViewById<EditText>(R.id.matchNameField)
-        val matchText = matchNameField.text.toString()
+
+
 
 
 
@@ -29,9 +29,13 @@ class MainActivity : AppCompatActivity() {
         val yourNameField = findViewById<EditText>(R.id.yourNameField)
         val opponentNameField = findViewById<EditText>(R.id.opponentNameField)
 
+        val mainActivityIntent = Intent(this, MainActivity::class.java)
+
 
         nextButton.setOnClickListener {
-            
+            val matchText = matchNameField.text.toString()
+            mainActivityIntent.putExtra("MatchText", matchText)
+            startActivity(mainActivityIntent)
         }
 
 
